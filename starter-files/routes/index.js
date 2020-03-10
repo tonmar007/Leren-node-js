c12db899186c719a9afe28cf92cb8f1d33cb7327
tonmar007 <tonmar005@gmail.com>
2020-03-10 23:48:46 +0100

    The second version

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 // Do work here
 /*router.get('/', (req, res) => {
